c814ab299c5b74051a081fd4073d264a04767361
Ames <ameyers@flowroute.com>
2017-02-27 20:07:58 -0800

    update functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitor(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_retrieve_it_laser(self):
+		#Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://127.0.0.1:8000')
+
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+		# She is invited to enter a to-do item straight away
+
+if __name__ == "__main__":
+	unittest.main()
